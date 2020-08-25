e602e65a868ee60d72a7f4f8affa741ea9cf57ee
李栋焱 <lidongyan@MacBook-Pro.local>
2020-08-25 18:32:15 +0800

    feat: github发布了包

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
+
 export * from "./components/button";
 export * from "./components/icon";
 export * from "./components/avatar";
+export * from "./components/badge"
+export * from "./components/carousel"
+export * from "./components/radio"
+
+
 
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
 export * from "./components/shared/icons";
 export * from "./components/shared/animation";
+
