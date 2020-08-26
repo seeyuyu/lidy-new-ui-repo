7cdf9692209f2d51ab60af7b6df9089e80be5cc5
seeyuyu <seeyuyu@163.com>
2020-08-26 06:36:30 +0000

    Deploying to gh-pages from  @ 273fc11b7a4e6f62f6ba8211dc7aa01764795708 🚀

---
+++ precache-manifest.3fa01920e3ce865b93bdf3b26dcbc48d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0365ea1946008e05fe95849d9721a704",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.55baa6b6065a270b55c5.bundle.js"
+  },
+  {
+    "url": "runtime~main.55baa6b6065a270b55c5.bundle.js"
+  },
+  {
+    "url": "vendors~main.55baa6b6065a270b55c5.bundle.js"
+  },
+  {
+    "url": "vendors~main.55baa6b6065a270b55c5.bundle.js.LICENSE.txt"
+  }
+]);
